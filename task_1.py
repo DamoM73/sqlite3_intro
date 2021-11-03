309339dfd8c1e83005d07c6da53c2087e085cca1
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2021-11-03 14:12:26 +1000

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,12 +1,17 @@
 import sqlite3
+user_input = input("Enter Country: ")
 
 with sqlite3.connect("movies.db") as db:
     cursor = db.cursor()
     cursor.execute(
-        """
+        f"""
         SELECT dirname
         FROM director
+        WHERE country = '{user_input}'
         """
     )
 
-    print(cursor.fetchall())
+    results = cursor.fetchall()
+
+    for record in results:
+        print(record[0])
